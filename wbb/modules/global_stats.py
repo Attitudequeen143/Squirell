a1c98f140a6df60b07ae7e757cb6a7ec43680587
Attitudequeen143 <105151127+Attitudequeen143@users.noreply.github.com>
2022-05-16 12:29:08 +0530

    Update global_stats.py

--- wbb/modules/global_stats.py
+++ wbb/modules/global_stats.py
@@ -103,8 +103,8 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
-    rurl = "https://github.com/thehamkercat/williambutcherbot"
+    url = "https://api.github.com/repos/Attitudequeen143/Squirell/contributors"
+    rurl = "https://github.com/Attitudequeen143/Squirell"
     developers = await get(url)
     commits = 0
     for developer in developers:
